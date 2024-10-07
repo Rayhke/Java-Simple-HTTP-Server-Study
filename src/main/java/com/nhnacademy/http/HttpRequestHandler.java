4e594b0ff63d769cf7c85caa9041b671298f3840
nink2458 <nink2458@naver.com>
2024-10-07 18:32:47 +0900

    이전의 HttpRequestHandler 구조를 Runnable 로 변환하여, Thread 작업으로 수행

--- src/main/java/com/nhnacademy/http/HttpRequestHandler.java
+++ src/main/java/com/nhnacademy/http/HttpRequestHandler.java
@@ -9,67 +9,120 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.util.LinkedList;
 import java.util.Objects;
+import java.util.Queue;
 
 @Slf4j
 public class HttpRequestHandler implements Runnable {
 
-    private final Socket client;
+    private static final int MAX_QUEUE_SIZE = 10;
 
-    public HttpRequestHandler(Socket client) {
-        if (Objects.isNull(client) || client.isClosed()) {
-            throw new IllegalArgumentException("client is Null!");
-        }
-        this.client = client;
+    private final Queue<Socket> requestQueue;
+
+    private final int maxQueueSize;
+
+    public HttpRequestHandler() {
+        this(MAX_QUEUE_SIZE);
     }
+
+    public HttpRequestHandler(int maxQueueSize) {
+        if (maxQueueSize < 1) {
+            throw new IllegalArgumentException(
+                    String.format("maxQueueSize is range Out! : %d", maxQueueSize)
+            );
+        }
+
+        this.requestQueue = new LinkedList<>();
+        this.maxQueueSize = maxQueueSize;
+    }
+
+    // =================================================================================================================
+    // method
+
+    public synchronized void addRequest(Socket client) {
+        while (maxQueueSize <= requestQueue.size()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                log.error("{}", e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        requestQueue.add(client);
+        notify();
+    }
+
+    public synchronized Socket getRequest() {
+        while (requestQueue.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                log.error("{}", e.getMessage(), e);
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        notify();
+        return requestQueue.poll();
+    }
+
+    // =================================================================================================================
+    // thread
 
     @Override
     public void run() {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))
-        ) {
-            StringBuilder requestBuilder = new StringBuilder();
-            log.debug("------HTTP-REQUEST_start()");
-            while (true) {
-                String line = bufferedReader.readLine();
-                requestBuilder.append(line);
-                log.debug("{}", line);
+        while (true) {
+            Socket client = getRequest();
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                 BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()))
+            ) {
+                StringBuilder requestBuilder = new StringBuilder();
+                log.debug("------HTTP-REQUEST_start()");
+                while (true) {
+                    String line = bufferedReader.readLine();
+                    requestBuilder.append(line);
+                    log.debug("{}", line);
 
-                if (StringUtils.isNullOrEmpty(line)) { break; }
+                    if (StringUtils.isNullOrEmpty(line)) {
+                        break;
+                    }
+                }
+                log.debug("------HTTP-REQUEST_end()");
+
+                StringBuilder responseBody = new StringBuilder();
+                responseBody.append("<html>");
+                responseBody.append("<body>");
+                responseBody.append(String.format("<h1>[%s] : Hello Java</h1>", Thread.currentThread().getName()));
+                responseBody.append("</body>");
+                responseBody.append("</html>");
+
+                StringBuilder responseHeader = new StringBuilder();
+                responseHeader.append(String.format("HTTP/1.0 200 OK%s", StringUtils.CRLF));
+                responseHeader.append(String.format("Server: HTTP server/0.1%s", StringUtils.CRLF));
+                responseHeader.append(String.format("Content-type: text/html; charset=%s%s", "UTF-8", StringUtils.CRLF));
+                responseHeader.append(String.format("Connection: Closed%s", StringUtils.CRLF));
+                responseHeader.append(String.format("Content-Length:%d %s%s", responseBody.toString().getBytes().length, StringUtils.CRLF, StringUtils.CRLF));
+
+                bufferedWriter.write(responseHeader.toString());
+                bufferedWriter.write(responseBody.toString());
+                bufferedWriter.flush();
+
+                log.debug("header : {}", responseHeader);
+                log.debug("body : {}", responseBody);
+
+            } catch (IOException e) {
+                log.error("socket error : {}", e.getMessage(), e);
+            } finally {
+                close(client);
             }
-            log.debug("------HTTP-REQUEST_end()");
-
-            StringBuilder responseBody = new StringBuilder();
-            responseBody.append("<html>");
-            responseBody.append("<body>");
-            responseBody.append("<h1>Hello Java</h1>");
-            responseBody.append("</body>");
-            responseBody.append("</html>");
-
-            StringBuilder responseHeader = new StringBuilder();
-            responseHeader.append(String.format("HTTP/1.0 200 OK%s", StringUtils.CRLF));
-            responseHeader.append(String.format("Server: HTTP server/0.1%s", StringUtils.CRLF));
-            responseHeader.append(String.format("Content-type: text/html; charset=%s%s", "UTF-8", StringUtils.CRLF));
-            responseHeader.append(String.format("Connection: Closed%s", StringUtils.CRLF));
-            responseHeader.append(String.format("Content-Length:%d %s%s", responseBody.toString().getBytes().length, StringUtils.CRLF, StringUtils.CRLF));
-
-            bufferedWriter.write(responseHeader.toString());
-            bufferedWriter.write(responseBody.toString());
-            bufferedWriter.flush();
-
-            log.debug("header : {}", responseHeader);
-            log.debug("body : {}", responseBody);
-
-        } catch (IOException e) {
-            log.error("socket error : {}", e.getMessage(), e);
-        } finally {
-            close();
         }
     }
 
     // =================================================================================================================
 
-    private void close() {
+    private void close(Socket client) {
         try {
             if (Objects.nonNull(client)
                     && !client.isClosed()) client.close();
--- src/main/java/com/nhnacademy/http/SimpleHttpServer.java
+++ src/main/java/com/nhnacademy/http/SimpleHttpServer.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 public class SimpleHttpServer {
@@ -13,7 +14,7 @@ public class SimpleHttpServer {
 
     private final int port;
 
-    private final ServerSocket serverSocket;
+    private final AtomicLong atomicCounter;
 
     public SimpleHttpServer() {
         this(DEFAULT_PORT);
@@ -23,23 +24,39 @@ public class SimpleHttpServer {
         if (port < 0 || 65535 < port) {
             throw new IllegalArgumentException(String.format("port is range Out! : %d", port));
         }
-        this.port = port;
 
-        try {
+        this.port = port;
+        this.atomicCounter = new AtomicLong();
+        /*try {
             serverSocket = new ServerSocket(this.port);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     // =================================================================================================================
 
     public synchronized void start() throws IOException {
-        try {
-            while (!Thread.currentThread().isInterrupted()) {
+        try (ServerSocket serverSocket = new ServerSocket(port)
+        ) {
+            HttpRequestHandler httpRequestHandlerA = new HttpRequestHandler();
+            HttpRequestHandler httpRequestHandlerB = new HttpRequestHandler();
+
+            Thread threadA = new Thread(httpRequestHandlerA);
+            threadA.setName("threadA");
+            threadA.start();
+
+            Thread threadB = new Thread(httpRequestHandlerB);
+            threadB.setName("threadB");
+            threadB.start();
+
+            while (true) {
                 Socket client = serverSocket.accept();
-                Thread thread = new Thread(new HttpRequestHandler(client));
-                thread.start();
+                if ((atomicCounter.getAndDecrement() & 1) == 0) {
+                    httpRequestHandlerA.addRequest(client);
+                } else {
+                    httpRequestHandlerB.addRequest(client);
+                }
             }
         } catch (Exception e) {
             log.debug("{}", e.getMessage(), e);
