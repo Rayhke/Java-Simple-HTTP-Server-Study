c1522ca3133364cb69e15e85e705279bbd2ea979
nink2458 <nink2458@naver.com>
2024-10-08 11:26:30 +0900

    commit

--- src/test/java/com/nhnacademy/http/channel/RequestChannelTest.java
+++ src/test/java/com/nhnacademy/http/channel/RequestChannelTest.java
@@ -14,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Slf4j
 class RequestChannelTest {
 
+    private static final long DEFAULT_QUEUE_SIZE = 10L;
+
     @DisplayName("default queueSize : 10")
     @Test
     void constructorTest1() throws Exception {
@@ -22,23 +24,24 @@ class RequestChannelTest {
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "queueSize", requestChannel);
         long queueSize = (long) readFieldValue.get();
 
-        //TODO#113 기본 생성자를 이용해서 생성된 requestChannel의 queueSize가 10인지 검증 합니다.
-
+        log.debug("default queueSize : {}", queueSize);
+        Assertions.assertEquals(DEFAULT_QUEUE_SIZE, queueSize);
     }
 
     @DisplayName("queueSize = -5")
     @Test
     void constructorTest2() {
-        //TODO#114 RequestChannel 객체 생성시 queueSize -5 이면 IllegalArgumentException 발생하는지 검증 합니다.
-
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new RequestChannel(-5));
     }
 
     @DisplayName("addRequest : 5 times")
     @Test
     void addRequest_5_times() throws Exception {
         RequestChannel requestChannel = new RequestChannel();
-        //TODO#115 requestChannel에 5개의 아무것도 실행하지 않는 작업을(Executable) 등록 합니다. Executable : ()->{} 사용합니다.
-
+        // requestChannel 에 5개의 아무것도 실행하지 않는 작업을 (Executable) 등록 합니다. Executable : () -> {} 사용합니다.
+        for (int n = 0; n < 5; n++) {
+            requestChannel.addHttpJob(() -> {});
+        }
 
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "requestQueue", requestChannel);
         Queue queue = (Queue) readFieldValue.get();
@@ -52,18 +55,22 @@ class RequestChannelTest {
 
         RequestChannel requestChannel = new RequestChannel(10);
 
-        //TODO#116 requestChannel에 11개의 빈 작업을 등록하는 thread를 구현 하세요. 빈 작업: ()->{}
-        Thread thread = null;
-
+        // requestChannel 에 11개의 빈 작업을 등록하는 thread 를 구현 하세요. 빈 작업 : () -> {}
+        Thread thread = new Thread(() -> {
+            for (int n = 0; n < 11; n++) {
+                requestChannel.addHttpJob(() -> {});
+            }
+        });
         thread.start();
         Thread.sleep(2000);
 
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "requestQueue", requestChannel);
         Queue queue = (Queue) readFieldValue.get();
 
-        //requestChannel의 queueSize =10, 11번재 executable 객체를 추가할 수 없어 대기 함니다.
+        //requestChannel 의 queueSize = 10, 11번재 executable 객체를 추가할 수 없어 대기 함니다.
         log.debug("11-queueSize : {}", queue.size());
         Assertions.assertEquals(10, queue.size());
+
         thread.interrupt();
     }
 
@@ -71,12 +78,12 @@ class RequestChannelTest {
     @Test
     void getRequest() throws Exception {
         RequestChannel requestChannel = new RequestChannel(10);
-        for (int i = 1; i <= 5; i++) {
-            requestChannel.addHttpJob(() -> {
-            });
+        for (int i = 0; i < 5; i++) {
+            requestChannel.addHttpJob(() -> {});
         }
-        //TODO#117 requestChannel 작업을 할당 받아 실행 하세요.
-        Executable executable = null;
+
+        Executable executable = requestChannel.getHttpJob();
+        executable.execute();
 
         Try<Object> readFieldValue = ReflectionUtils.tryToReadFieldValue(RequestChannel.class, "requestQueue", requestChannel);
         Queue queue = (Queue) readFieldValue.get();
@@ -91,16 +98,15 @@ class RequestChannelTest {
     void getRequest_from_empty_queue() throws InterruptedException {
         RequestChannel requestChannel = new RequestChannel(10);
 
-        Thread thread = new Thread(() -> requestChannel.getHttpJob());
+        Thread thread = new Thread(requestChannel::getHttpJob);
+        // Thread thread = new Thread(() -> requestChannel.getHttpJob());
         thread.setName("my-thread");
         thread.start();
 
         Thread.sleep(2000);
 
         log.debug("{} : {}", thread.getName(), thread.getState());
-
-        //TODO#118 thread의 상태가 WAITING 상태인지 검증 합니다.
-
+        Assertions.assertEquals(Thread.State.WAITING, thread.getState());
 
         thread.interrupt();
     }
